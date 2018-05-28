0f50b9d01f38738db9c0fe9b86b7a95fe9c902ae
hudson-ayers <hayers@stanford.edu>
2018-05-28 02:34:21 -0700

    Django serves all static pages exactly as they were served by nginx. Firebase still included

--- mysite/pages/urls.py
+++ mysite/pages/urls.py
@@ -2,5 +2,14 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url('^$', TemplateView.as_view(template_name='index2.html')),
+    url('^$', TemplateView.as_view(template_name='index2.html')), #TODO: Replace with index.html
+    url('index.html', TemplateView.as_view(template_name='index.html')),
+    url('player.html', TemplateView.as_view(template_name='player.html')),
+    url('index_dev.html', TemplateView.as_view(template_name='index_dev.html')),
+    url('widget.html', TemplateView.as_view(template_name='widget.html')),
+    url('index.js', TemplateView.as_view(template_name='index.js')),
+    url('player.js', TemplateView.as_view(template_name='player.js')),
+    url('config.js', TemplateView.as_view(template_name='config.js')),
+    url('puffer.js', TemplateView.as_view(template_name='puffer.js')),
+    url('style.css', TemplateView.as_view(template_name='style.css')),
 ]
