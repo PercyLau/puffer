339cbcbc2273a671dbc87f7f3090e1cf2b4f2095
hudson-ayers <hayers@stanford.edu>
2018-05-29 16:48:36 -0700

    Aesthetic improvements and removing firebase auth

--- mysite/pages/static/player.js
+++ mysite/pages/static/player.js
@@ -109,10 +109,7 @@ function setup_control_bar() {
 }
 
 function init_app() {
-  /* Listening for auth state changes */
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      /* User is signed in */
+    if (true) {
       document.getElementById('user-signed-in').style.display = 'block';
       document.getElementById('user-info').textContent = 'Welcome! ' + user.displayName;
 
@@ -140,17 +137,9 @@ function init_app() {
           start_dashjs(user, aid);
         }
       }
-    } else {
-      /* Redirect to the sign-in page if user is not signed in */
-      window.location.replace('/widget.html');
     }
   });
 
-  document.getElementById('sign-out').addEventListener('click',
-    function() {
-      firebase.auth().signOut();
-    }
-  );
 }
 
 window.addEventListener('load', init_app);
