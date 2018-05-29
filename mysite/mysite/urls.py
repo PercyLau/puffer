d090b7c068b8779d26710c39bfbd501012214bb5
hudson-ayers <hayers@stanford.edu>
2018-05-29 16:15:48 -0700

    Fix player.html to reflect that you do not need to be an admin to access the page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
 #Add Django site authentication urls (for login, logout, password management)
 urlpatterns += [
     url(r'accounts/', include('django.contrib.auth.urls')),
+    #url(r'^invitations/', include('invitations.urls', namespace='invitations')),
 ]
