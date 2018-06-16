cb9907b7a7a6bb062daf0f137063e02fda694a73
Puffer <puffer@puffer.stanford.edu>
2018-06-16 03:37:08 +0000

    Add puffer.stanford.edu as an allowed host

--- src/portal/portal/settings.py
+++ src/portal/portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['PUFFER_PORTAL_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['171.64.65.88']
+ALLOWED_HOSTS = ['171.64.65.88', 'puffer.stanford.edu']
 
 
 # Application definition
